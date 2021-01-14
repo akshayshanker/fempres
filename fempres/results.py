22a6d029de1d2518e7b2a9e344d6afe9790d4dd3
akshayshanker <akshay.shanker@me.com>
2021-01-14 18:59:01 +1100

    results updated

--- fempres/results.py
+++ fempres/results.py
@@ -14,13 +14,189 @@ import copy
 import sys
 import importlib
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import collections
+import pandas as pd
+
 
 # Education model modules
 import edumodel 
 from solve_policies.studysolver import generate_study_pols
 
-def plot_results():
+def plot_results(moments_sim_all,
+				 moments_data_all,
+				 variable_list,
+				 group_list):
+
+	line_names =['data', 'sim']
+	linestyles=["-","-"]
+	col_dict = {'data': 'black', 'sim':'gray'}
+	markers=['x', 'o']
+
+	# loop through each of the variables in the list
+	for i, name in zip(np.arange(len(variable_list)),variable_list):
+		# create a plot for this variable
+		fig, ax = plt.subplots(4,2)
+		ax = ax.flatten()
+
+		#loop through each of the group
+		for j, group_id in zip(np.arange(len(group_list)),group_list):
+			xs = np.arange(11)
+			ys = moments_data_all[j,:,i]
+			p = ax[j].plot(xs, ys, marker=markers[0], color=col_dict['data'], linestyle=linestyles[0],
+						label=line_names[0], linewidth=2)
+			ys = moments_sim_all[j,:,i]
+			p = ax[j].plot(xs, ys, marker=markers[1], color=col_dict['sim'], linestyle=linestyles[1],
+			label=line_names[1], linewidth=2)
+
+			ax[j].set_xlabel('Week')
+			ax[j].set_title(group_id)
+			#ax[j].set_ylim(ylim)
+			ax[j].spines['top'].set_visible(False)
+			ax[j].spines['right'].set_visible(False)
+			#ax[j].legend(loc='upper left', ncol=2)
+		
+		handles, labels = ax[7].get_legend_handles_labels()
+		fig.legend(handles, labels, loc='upper center', ncol=2)
+		fig.savefig("plot_test/{}.png".format(name +'_'+group_id), transparent=True)
+
+
 	return 
+
+def make_table(params, param_names_old, param_names_new):
+
+	#Row names for the table, headers (i.e. no values) commented out
+	table_row_names = [
+	    "Course grade utility weight",
+	    #"Exponential discount factor",
+	    "\hspace{0.4cm}Discount factor mean",
+	    "\hspace{0.4cm}Discount factor persistence",
+	    "\hspace{0.4cm}Discount factor std. deviation",
+	    "Hyperbolic discount factor",
+	    #"Study effectiveness for knowledge creation",
+	    "\hspace{0.4cm}Time solving MCQs",
+	    "\hspace{0.4cm}Time earning happiness units",
+	    "\hspace{0.4cm}Time answering SAQs",
+	    "\hspace{0.4cm}Time studying the textbook",
+	    "\hspace{0.4cm}Time solving MCQs \& earning happiness",
+	    "\hspace{0.4cm}Time solving MCQs \& SAQ",
+	    "\hspace{0.4cm}Time solving MCQs \& studying textbook",
+	    "\hspace{0.4cm}Time earning happiness \& solving SAQs",
+	    "\hspace{0.4cm}Time earning happiness \& studying textbook",
+	    "\hspace{0.4cm}Time solving SAQs \& studying textbook",
+	    #"Final exam ability",
+	    "\hspace{0.4cm}Real exam ability mean",
+	    "\hspace{0.4cm}Real exam ability std. deviation",
+	    "\hspace{0.4cm}Perceived exam ability mean",
+	    "\hspace{0.4cm}Perceived exam ability std. deviation",
+	    "\hspace{0.4cm}Exam difficulty parameter"
+	    "\hspace{0.4cm}MCQ question difficulty parameter"
+	]
+
+	#Latex symbols for table row names, prefix r for raw string, or else causes unicode error
+	table_row_symbols = [
+	    r"$\alpha$",
+	    r"$\bar{\beta}$",
+	    r"$\rho_{\beta}$",
+	    r"$\sigma_{\beta}$",
+	    r"$\delta$",
+
+	    r"$\gamma^{mcq}$",
+	    r"$\gamma^{sim}$",
+	    r"$\gamma^{saq}$",
+	    r"$\gamma^{book}$",
+	    r"$\gamma^{mcq\&sim}$",
+	    r"$\gamma^{mcq\&saq}$",
+	    r"$\gamma^{mcq\&book}$",
+	    r"$\gamma^{sim\&saq}$",
+	    r"$\gamma^{sim\&book}$",
+	    r"$\gamma^{saq\&book}$",
+
+	    r"$\xi$",
+	    r"$\sigma_{\varepsilon^\xi}$",
+	    r"$\xi^*$",
+	    r"$\sigma_{\varepsilon^\xi{^*}}$",
+	    r"$\lambda^E$"
+	    r"$\varphi$"
+	]
+
+	#Convert numpy array to a list of lists
+	A_list = params.tolist()
+
+	#Reorder parameter values according to param_names_new and store in new list of lists
+	A_list_new = []
+	for lst in A_list:
+	    dict_old = collections.OrderedDict(zip(param_names_old, lst)) 
+	    dict_new = collections.OrderedDict.fromkeys(param_names_new)
+	    for key in dict_new.keys():
+	        dict_new[key] = dict_old[key]
+	    A_list_new.append(list(dict_new.values()))
+
+	#Reorder elements of list to match male-female column orders in the table (means only)
+	mf_order = [1, 0, 3, 2, 5, 4, 7, 6]
+	A_list_new[:] = [A_list_new[i] for i in mf_order]
+
+	#Combine the numbers with row names and symbols. Note, because of mismatch between parameter list
+	#given and parameter list in the pdf, there will be NaNs/Nones in the dataframe df.
+	A_list_new.insert(0, table_row_symbols)
+	A_list_new.insert(0, table_row_names)
+	pd.set_option('display.max_colwidth', None) #Stop long strings being cut off with ...
+	df = pd.DataFrame(A_list_new).transpose()
+
+	#Convert list to tex code, 3 decimal places, no row numbering
+	ncols = len(A_list_new)
+	col_format = "l" + ncols*"c" #note the extra centered cell for formatting purposes
+
+	table_tex = df.to_latex(
+	    header = False,
+	    float_format="{:0.3f}".format, 
+	    column_format=col_format,
+	    escape = False, #stops reading of latex command symbols as text (eg. $ -> \$, \ -> \backslash)
+	    index = False)
+
+	#Add code before/after tabular environment
+	table_env_start = [
+	    r"\begin{table}[htbp]",
+	    r"\caption{\textsc{Parameter estimates}}\label{table:estimates}",
+	    r"\centering"
+	]
+	table_env_end = r"\end{table}"
+	table_tex = "\n".join(table_env_start) + "\n" + table_tex + table_env_end
+	table_tex = table_tex.replace(r"\bottomrule", r"\hline" + "\n" + r"\hline")
+
+
+	#Add code for multi-level column headings
+	headings = [
+	    r"& & \multicolumn{4}{c}{Undisclosed Gender} &\multicolumn{4}{c}{Disclosed Gender}\\",
+	    r"\cmidrule(l{0.2cm}r{0.3cm}){3-6}\cmidrule(l{0.2cm}r{0.3cm}){7-10}",
+	    r"& & \multicolumn{2}{c}{Males} &\multicolumn{2}{c}{Females} & \multicolumn{2}{c}{Males} &\multicolumn{2}{c}{Females}\\",
+	    r"\cmidrule(l{0.2cm}r{0.3cm}){3-4}\cmidrule(l{0.2cm}r{0.3cm}){5-6}\cmidrule(l{0.2cm}r{0.3cm}){7-8} \cmidrule(l{0.2cm}r{0.3cm}){9-10}",
+	    r"& & Estimates  & S.E.  & Estimates  & S.E. & Estimates  & S.E. & Estimates  & S.E. & \tabularnewline",
+	    r" &  &  (1) & (2) & (3)  & (4) & (5) & (6) & (7) & (8) \\",
+	    r"\hline",
+	    r"\\",
+	    r"\textit{\textbf{Panel A: Male President}} & & & & & & & & & \\",
+	]
+	table_tex = table_tex.replace(r"\toprule", r"\hline" + "\n" + r"\hline" + "\n".join(headings) + "\n")
+
+
+	#Add multi-level row-headings
+	row_headings = [
+	    r"Exponential discount factor & & & & & & & & & \\" + "\n",
+	    r"\hline" + "\n" + r"Study effectiveness for knowledge creation & & & & & & & & & \\" + "\n",
+	    r"\hline" + "\n" +  r"Final exam ability & & & & & & & & & \\" + "\n"
+	]
+	row_heading_locations = [
+	    "\hspace{0.4cm}Discount factor mean",
+	    "\hspace{0.4cm}Time solving MCQs",
+	    "\hspace{0.4cm}Real exam ability mean"
+	]
+
+	for i in range(0, 3):
+	    table_tex = table_tex.replace(row_heading_locations[i], row_headings[i] + row_heading_locations[i], 1)
+
+	print(table_tex)
 
 if __name__ == "__main__":
 
@@ -82,7 +258,7 @@ if __name__ == "__main__":
 							U,
 							U_z,
 							random_draw = True,
-							uniform = True,
+							uniform = False,
 							param_random_means = sampmom[0], 
 							param_random_cov = sampmom[1], 
 							random_bounds = param_random_bounds)
@@ -96,54 +272,125 @@ if __name__ == "__main__":
 	# Now gather all the moments to the master processor (rank 0)
 	if world.rank == 0:
 		
-		moments_sim_all =  np.array(moments_sim_all)
+		moments_sim_all  =  np.array(moments_sim_all)
 		param_all = np.array(param_all)
+		moments_data_all = np.empty(np.shape(moments_sim_all))
 
+		# re-format data moments so in the same format as sim moments, combined
 
-		np.save('moments_all',moments_sim_all )
-		np.save('param_all', param_all)
+		for i, keys in zip(np.arange(8),list(moments_data_mapped.keys())):
+			moments_data_all[i] = moments_data_mapped[keys]['data_moments']
+
+		# Generate new data-frames for plotting 
+
+		#np.save('moments_all',moments_sim_all )
+		#np.save('param_all', param_all)
 
 		# Each item in moment list is a 11 x 36 array
 		# The rows are are 
 
-		list_moments = ['Final exam mark',\
-						'Overall course grade',\
-						'Expected final exam mark',\
-						'Expected final exam mark (other)',\
-						'Game session hours',\
-						'E-book hours',\
-						'MCQ session hours',\
-						'SAQ session hours',\
+		list_moments = ['av_final',\
+						'av_mark',\
+						'av_markw13_exp1',\
+						'av_markw13_exp2',\
+						'av_game_session_hours_cumul',\
+						'av_ebook_session_hours_cumul',\
+						'av_mcq_session_hours_cumul',\
+						'av_saq_session_hours_cumul',\
 						'av_player_happy_deploym_cumul',\
 						'av_mcq_attempt_nonrev_cumul',\
-						'SAQ attempts', \
+						'av_sa_attempt_cumul', \
 						'av_mcq_Cshare_nonrev_cumul',\
-						'SD final exam mark',\
-						'SD Overall course grade',\
-						'SD Expected final exam mark',\
-						'SD Expected final exam mark (other)',\
-						'SD Game session hours',\
-						'SD E-book hours',\
-						'SD MCQ session hours',\
-						'SD SAQ session hours',\
+						'sd_final',\
+						'sd_mark',\
+						'sd_markw13_exp1',\
+						'sd_markw13_exp2',\
+						'sd_game_session_hours_cumul',\
+						'sd_ebook_session_hours_cumul',\
+						'sd_mcq_session_hours_cumul',\
+						'sd_saq_session_hours_cumul',\
 						'sd_player_happy_deploym_cumul',\
 						'sd_mcq_attempt_nonrev_cumul',\
-						'SD SAQ attempts', \
+						'sd_sa_attempt_cumul', \
 						'sd_mcq_Cshare_nonrev_cumul',\
-						'AC game session hours',\
-						'AC e-book session hours',\
-						'AC MCQ session hours',\
-						'AC SAQ session hours',\
+						'acgame_session_hours',\
+						'acebook_session_hours',\
+						'acmcq_session_hours',\
+						'acsaq_session_hours',\
 						'acmcq_Cshare_nonrev',\
-						'Corr. MCQ and SAQ hours',\
-						'Corr. game and SAQ hours',\
-						'Corr. game and MCQ hours',\
-						'Corr. e-book and SAQ hours',\
-						'Corr. e-book. and MCQ hours',\
-						'Corr. e-book and game hours',\
-						'Corr. ATAR and final exam mark']
+						'acmcq_Cattempt_nonrev',\
+						'cmcsaq_session_hours',\
+						'cgsaq_session_hours',\
+						'cgmcq_session_hours',\
+						'cesaq_session_hours',\
+						'cemcq_session_hours',\
+						'ceg_session_hours',\
+						'c_atar_ii']
+
+
+		param_names_old = [
+				    'beta_bar',
+				    'rho_beta',
+				    'sigma_beta',
+				    'delta',
+				    'zeta_star',
+				    'sigma_zeta',
+				    'zeta_hstar',
+				    'sigma_hzeta',
+				    'alpha',
+				    'lambda_E',
+				    'gamma_1',
+				    'gamma_2',
+				    'gamma_3',
+				    'gamma_4',
+				    'gamma_12',
+				    'gamma_21',
+				    'gamma_34',
+				    'gamma_13',
+				    'gamma_22',
+				    'gamma_23',
+				    'gamma_33',
+				    'gamma_11',
+				    'd',
+				    'varphi']
+
+		param_names_new = [
+			    'd',
+
+			    'alpha',
+			    'beta_bar',
+			    'rho_beta',
+			    'sigma_beta',
+			    'delta',
+
+			    'gamma_3',
+			    'gamma_4',
+			    'gamma_1',
+			    'gamma_2',
+			    'gamma_34',
+			    'gamma_13',
+			    'gamma_12',
+
+			    #Gamma symbols below do not match table
+			    'gamma_21',
+			    'gamma_22',
+			    'gamma_23',
+			    'gamma_33',
+			    'gamma_11',
+
+			    'zeta_star',
+			    'sigma_zeta',
+			    'zeta_hstar',
+			    'sigma_hzeta',
+			    'lambda_E',
+			    'varphi'
+			]
 
 		#list_params = param_random_bounds.keys()
+		#make_table(param_all,param_names_old,param_names_new)
+		plot_results(moments_sim_all,moments_data_all, list_moments, list(moments_data_mapped.keys()))
+
+
 
 
 
